608d2317102ecdea602da2b957bfbc887571944b
Mifimbar12 <mifimbar@gmail.com>
2024-11-09 12:18:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "L7YD3bAR#DTiZ6-kbN6dAqu2lvGfakKLq6VWUX6lQ7cOe3hNtVOw",
+SESSION_ID: process.env.SESSION_ID || "OqxgiB6R#NaIHYAaeJ8vfZIE3eCQdWZ1E1kjz85mEZarKJ6enqAU",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
+AUTO_REPLY: process.env.AUTO_REPLY || " true",
 };
